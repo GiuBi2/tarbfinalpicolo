a1a627b1e5772bfbc36ccdb63aa87d3dc0fada2f
Giulia Bianca <86010339+GiuBi2@users.noreply.github.com>
2022-12-01 12:31:54 +0000

    feita a conexão certa

--- index.js
+++ index.js
@@ -14,29 +14,30 @@ app.set('view engine', 'ejs');
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(cookieParser());
 
 
-app.get('/listar', async function(req, res){
+app.get('/listar', async function(req, res) {
   const produtos = await produto.findAll();
   res.json(produtos);
 
 })
 
-app.get('/cadastrar', async function(req, res){
+app.get('/cadastrar', async function(req, res) {
   res.render('cadastrar');
 })
-app.get('/', async function(req, res){
+app.get('/', async function(req, res) {
   res.render("home")
 })
 
 
 app.post('/cadastrar', async function(req, res) {
-  const produto = await produto.create(req.body)
-  res.json(produto)
+  const _produto = produto.create(req.body)
+  console.log(_produto)
+  res.json(_produto)
 })
 
 app.listen(3000, function() {
